5ca58f9b08887739ac89d9b1bd020d0a689d8678
mchibok <m.chibok@gmail.com>
2019-11-14 18:58:23 -0500

    modif script - test difference problème 1500

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 
 # nom de la couche à séparer
-nom_couche = "isochrone_2019-08-19T08_00"
+#nom_couche = "isochrone_2019-08-19T16_00_du_TLO_intervalles_5_max_60_marche_800"
+nom_couche = "test"
 
 # importation des éléments de pyqgis
 from qgis.core import *
@@ -19,67 +20,71 @@ array = []
 # 2. initialisation de la boucle de la plus grosse valeur à la plus petite
 # isochrone = i
 for i in range(len(values)):
-    # insère la valeur time en texte dans le vecteur array
-    array.append(str(values[i]))
-    # on fait les opérations suivantes seulement si ce n'est pas le plus petit isochrone
-    if values[i] != layer.minimumValue(idx):
-        # sélection entités inférieures à isochrone i
-        selection = "time<"+str(values[i])
-        expr = QgsFeatureRequest(QgsExpression(selection)).setFlags(QgsFeatureRequest.NoGeometry).setSubsetOfAttributes([])
-        it = layer.getFeatures(QgsFeatureRequest(expr))
-        ids = [id.id() for id in it]
-        layer.selectByIds(ids)
-        # dissolve/regroupement les entités inférieures à i
-        param_diss = {
-            "INPUT": QgsProcessingFeatureSourceDefinition(nom_couche, True),
-            "OUTPUT": 'TEMPORARY_OUTPUT'}
-        algo_diss = processing.run("qgis:dissolve", param_diss)["OUTPUT"]
-        # ajout de la couche fusionnée algo_diss des entités inférieures à i
-        QgsProject.instance().addMapLayer(algo_diss)
-        # sélection de l'isochrone i
-        selection = "time="+str(values[i])
-        expr = QgsFeatureRequest(QgsExpression(selection)).setFlags(QgsFeatureRequest.NoGeometry).setSubsetOfAttributes([])
-        it = layer.getFeatures(QgsFeatureRequest(expr))
-        ids = [id.id() for id in it]
-        layer.selectByIds(ids)
-        # différence de l'entité i et des entités inférieures à i (algo_diss)
-        params_diff = {
-            "INPUT": QgsProcessingFeatureSourceDefinition(nom_couche, True),
-            #"INPUT": "inf_i",
-            "OUTPUT": 'TEMPORARY_OUTPUT',
-            "OVERLAY": algo_diss.name()
-        }
-        algo_diff = processing.run("qgis:difference", params_diff)["OUTPUT"]
-        # enlever les entités fusionnées inférieures à i (algo_diss)
-        QgsProject.instance().removeMapLayer(algo_diss)
-        # nomme la couche différence avec la valeur time de l'isochrone i
-        algo_diff.setName(str(values[i]))
-        # ajoute cette couche au canevas
-        QgsProject.instance().addMapLayer(algo_diff)
-        # enlever toute sélection
-        layer.removeSelection()
-    # si l'isochrone i est le plus petit
-    elif values[i] == layer.minimumValue(idx):
-        # on le sélectionne
-        selection = "time="+str(values[i])
-        expr = QgsFeatureRequest(QgsExpression(selection)).setFlags(QgsFeatureRequest.NoGeometry).setSubsetOfAttributes([])
-        it = layer.getFeatures(QgsFeatureRequest(expr))
-        ids = [id.id() for id in it]
-        layer.selectByIds(ids)
-        # on en créée une couche en mémoire
-        min_iso = layer.materialize(QgsFeatureRequest().setFilterFids(layer.selectedFeatureIds()))
-        # nomme la couche avec la valeur time de l'isochrone i
-        min_iso.setName(str(values[i]))
-        # ajoute cette couche au canevas
-        QgsProject.instance().addMapLayer(min_iso)
-        # enlever toute sélection
-        layer.removeSelection()
+	# insère la valeur time en texte dans le vecteur array
+	#array.append(str(values[i]))
+	# on fait les opérations suivantes seulement si ce n'est pas le plus petit isochrone
+	#if values[i] == 600:
+	#if i < 8:
+	#if values[i] != layer.minimumValue(idx) and values[i] != 1500:
+	if values[i] != layer.minimumValue(idx):
+		array.append(str(values[i]))
+		# sélection entités inférieures à isochrone i
+		selection = "time<"+str(values[i])
+		expr = QgsFeatureRequest(QgsExpression(selection)).setFlags(QgsFeatureRequest.NoGeometry).setSubsetOfAttributes([])
+		it = layer.getFeatures(QgsFeatureRequest(expr))
+		ids = [id.id() for id in it]
+		layer.selectByIds(ids)
+		# dissolve/regroupement les entités inférieures à i
+		param_diss = {
+			"INPUT": QgsProcessingFeatureSourceDefinition(nom_couche, True),
+			"OUTPUT": 'TEMPORARY_OUTPUT'}
+		algo_diss = processing.run("qgis:dissolve", param_diss)["OUTPUT"]
+		# ajout de la couche fusionnée algo_diss des entités inférieures à i
+		QgsProject.instance().addMapLayer(algo_diss)
+		# sélection de l'isochrone i
+		selection = "time="+str(values[i])
+		expr = QgsFeatureRequest(QgsExpression(selection)).setFlags(QgsFeatureRequest.NoGeometry).setSubsetOfAttributes([])
+		it = layer.getFeatures(QgsFeatureRequest(expr))
+		ids = [id.id() for id in it]
+		layer.selectByIds(ids)
+		# différence de l'entité i et des entités inférieures à i (algo_diss)
+		params_diff = {
+			"INPUT": QgsProcessingFeatureSourceDefinition(nom_couche, True),
+			"OUTPUT": "TEMPORARY_OUTPUT",
+			"OVERLAY": algo_diss.name()
+		}
+		algo_diff = processing.run("qgis:difference", params_diff)["OUTPUT"]
+		# enlever les entités fusionnées inférieures à i (algo_diss)
+		QgsProject.instance().removeMapLayer(algo_diss)
+		# nomme la couche différence avec la valeur time de l'isochrone i
+		algo_diff.setName(str(values[i]))
+		# ajoute cette couche au canevas
+		QgsProject.instance().addMapLayer(algo_diff)
+		# enlever toute sélection
+		layer.removeSelection()
+	# si l'isochrone i est le plus petit
+	elif values[i] == layer.minimumValue(idx):
+		array.append(str(values[i]))
+		# on le sélectionne
+		selection = "time="+str(values[i])
+		expr = QgsFeatureRequest(QgsExpression(selection)).setFlags(QgsFeatureRequest.NoGeometry).setSubsetOfAttributes([])
+		it = layer.getFeatures(QgsFeatureRequest(expr))
+		ids = [id.id() for id in it]
+		layer.selectByIds(ids)
+		# on en créée une couche en mémoire
+		min_iso = layer.materialize(QgsFeatureRequest().setFilterFids(layer.selectedFeatureIds()))
+		# nomme la couche avec la valeur time de l'isochrone i
+		min_iso.setName(str(values[i]))
+		# ajoute cette couche au canevas
+		QgsProject.instance().addMapLayer(min_iso)
+		# enlever toute sélection
+		layer.removeSelection()
 
 # 3. fusion (merge) de toutes les couches distinctes des différents temps
 params_fusion = {
-    "LAYERS": array,
-    "OUTPUT": "TEMPORARY_OUTPUT"
-    }
+	"LAYERS": array,
+	"OUTPUT": "TEMPORARY_OUTPUT"
+}
 algo_fusion = processing.run("qgis:mergevectorlayers", params_fusion)["OUTPUT"]
 # 3.1 nomme la couche comme l'entrante en ajoutant '_séparé'
 algo_fusion.setName(nom_couche+"_séparé")
@@ -88,6 +93,6 @@ QgsProject.instance().addMapLayer(algo_fusion)
 
 # 4 ôte les couches temporaires
 for i in range(len(values)):
-    temp = QgsProject.instance().mapLayersByName(str(values[i]))[0]
-    QgsProject.instance().removeMapLayer(temp)
+	temp = QgsProject.instance().mapLayersByName(str(values[i]))[0]
+	QgsProject.instance().removeMapLayer(temp)
 
